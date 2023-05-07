f6b83923381c5fa212bfd546910baf56d5d6f21a
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2023-05-07 12:10:39 +0800

    Add an id-trans script in Awk

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 
 ```PowerShell
 mkdir .\out
-.\write8.ps1 .\out\n8.csv
+.\write8.ps1 .\out\id.csv
 ```
--- write8.ps1
+++ write8.ps1
@@ -2,4 +2,6 @@ param (
     [parameter()]
     [string] $FilePath
 )
-& swipl -s .\id-gen.prolog -g "go_1, halt." | Out-File -FilePath $FilePath
+$mid = ("{0}.n8" -f $FilePath)
+& swipl -s .\id-gen.prolog -g "go_1, halt." | Out-File -FilePath $mid
+& awk -f .\id-trans.awk $mid | Out-File -FilePath $FilePath
